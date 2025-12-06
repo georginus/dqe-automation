7569776090f315e6e229bb04005c8f7340f3bf1a
Heorhi_Chychenkau <georginus@gmail.com>
2025-12-06 18:00:38 +0400

    HW PyTest

---
+++ Selenium Introduction/main.py
@@ -0,0 +1,7 @@
+from table_interaction import extract_plotly_table_to_csv
+from doughnut_chart_interaction import doughnut_chart_interaction
+
+# Table
+extract_plotly_table_to_csv("report.html")
+# Doughnut Chart
+doughnut_chart_interaction("report.html")
